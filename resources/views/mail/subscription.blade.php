0ab87a17fd903b95c169db059dc605ca4b312dd5
Vlad Panin <paninvlad@mail.ru>
2023-04-27 15:21:31 +0400

    Laravel Project

---
+++ resources/views/mail/subscription.blade.php
@@ -0,0 +1,7 @@
+Уважаемый клиент, товар {{$sku->product->__('name')}} появился в наличии,
+<a href="{{route('sku', [$sku->product->category->code, $sku->product->code, $sku])}}">узнать подробности</a>
+<br>
+<br>
+<p>С уважением,</p>
+<p>Ваш Internet-Store Mvideo</p>
+<p>http://bestgadget.tech/</p>
